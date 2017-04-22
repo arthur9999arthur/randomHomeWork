98cd0488603819ecf8a878ef5b8d5d6cf4cf1be3
arthur9999arthur <arturivanov9999@prtonmail.com>
2017-04-22 17:58:42 +0900

    randomHomeWork v 0.1

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,28 +1,16 @@
-// Initialize app
 var myApp = new Framework7();
-
-
-// If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
-// Add view
 var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
 });
 
-// Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 });
 
-
-// Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
+myApp.onPageInit('work', function (page) {
     // Do something here for "about" page
-
 })
 
 // Option 2. Using one 'pageInit' event handler for all pages:
@@ -30,14 +18,28 @@ $$(document).on('pageInit', function (e) {
     // Get page data from event data
     var page = e.detail.page;
 
-    if (page.name === 'about') {
+    if (page.name === 'work') {
         // Following code will be executed for page with data-page attribute equal to "about"
         myApp.alert('Here comes About page');
     }
 })
 
 // Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+$$(document).on('pageInit', '.page[data-page="work"]', function (e) {
     // Following code will be executed for page with data-page attribute equal to "about"
     myApp.alert('Here comes About page');
 })
+
+var storedData = myApp.formGetData('my-form');
+
+var randomWork = document.querySelector('.randomWork');
+var firstPlayer = document.querySelector('.firstPlayer');
+var secondPlayer = document.querySelector('.secondPlayer');
+var players = [
+    firstPlayer.innerHTML,
+    secondPlayer.innerHTML,
+];
+randomWork.onclick = function(){
+    alert(players[namerand] + '  будет  ' + storedData.task1);
+};
+var namerand = Math.floor(Math.random() * players.length);
